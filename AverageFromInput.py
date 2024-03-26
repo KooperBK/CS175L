3e94fdca6a837c5f1116a9a63a73052b3013e0f6
Kooper <157034214+KooperBK@users.noreply.github.com>
2024-03-25 23:51:46 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,23 +2,27 @@
 #Kooper Kaney
 #AverageFromInput
 
-numbers = open("numbers.txt","r")
-
 def main():
     total = 0
     count = 0
     average = 0
-    try: 
+    try:
+        numbers = open("numbers.txt","r")
         for number in numbers:
-            number = int(number.strip())
-            count +=1
-            total +=number
-            print(f'I read in {count} number(s) Current number is:\t{number:6.2f} Total is:\t{total:6.2f}')
-            if count == 3:
-                average = total / count
-                print(f'Average: {average}')
+            try:
+                number = int(number.strip())
+                count +=1
+                total +=number
+                print(f'I read in {count} number(s) Current number is:\t{number:6.2f} Total is:\t{total:6.2f}')
+                if count == 3:
+                    average = total / count
+                    print(f'Average: {average}')
+            except ValueError as baddata:
+                print(f'Bad data. skipping {baddata}')
+
     except IOError:
         print('An error occured trying to read the file.')
+        numbers.close()
 
 if __name__ == '__main__':
     main()
